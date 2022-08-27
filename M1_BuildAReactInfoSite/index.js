2a7f49ab2666349b75b25a429138c2f0aa8103d6
erickariasec <erickarias1999@hotmail.com>
2022-08-27 17:57:03 -0500

    Scrimba, Learn React, Build a React Info Site, C004 - First React Practice

--- M1_BuildAReactInfoSite/index.js
+++ M1_BuildAReactInfoSite/index.js
@@ -1,5 +1,9 @@
-// Spend a minute memorizing the line below
-// Re-write the line of code as best you can from memory
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
 
-ReactDOM.render(<h2>My name is Erick!</h2>, document.getElementById("my-root-practice"))
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul><li>Dog</li><li>Cat</li><li>Rabbit</li></ul>,
+    document.getElementById("root")
+)
