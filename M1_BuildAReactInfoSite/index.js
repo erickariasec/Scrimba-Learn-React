c052ce35ad6ffe0c0d51b820bc3fe857b46a8231
erickariasec <erickarias1999@hotmail.com>
2022-08-27 17:49:21 -0500

    Scrimba, Learn React, Build a React Info Site, C003 - First React

---
+++ M1_BuildAReactInfoSite/index.js
@@ -0,0 +1,5 @@
+// Spend a minute memorizing the line below
+// Re-write the line of code as best you can from memory
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+ReactDOM.render(<h2>My name is Erick!</h2>, document.getElementById("my-root-practice"))
