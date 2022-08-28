10dd37bbcb422e5795acf3055dcec2a3625257a1
erickariasec <erickarias1999@hotmail.com>
2022-08-28 17:58:17 -0500

    Scrimba, Learn React, Build a React Info Site, vite-react, C024 - Navbar and Styling

--- M1_BuildAReactInfoSite/vite-react/src/components/Navbar.jsx
+++ M1_BuildAReactInfoSite/vite-react/src/components/Navbar.jsx
@@ -1,6 +1,17 @@
+import reactLogo from '../assets/react.svg';
+import "./Navbar.css";
+
 function Navbar() {
     return(
-        <h1>Navbar goes here</h1>
+        <nav className='nav'>
+            <div className='left'>
+                <img className='logo' src={reactLogo} />
+                <h3 className='logo-text'>ReactFacts</h3>
+            </div>
+            <div className="right">
+                <h4 className="project-title">React Course - Project 1</h4>
+            </div>
+        </nav>
     );
 }
 
