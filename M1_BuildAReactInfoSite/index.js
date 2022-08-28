4768604bfafcf9aa7679c3f1086b0c8b26abb76c
erickariasec <erickarias1999@hotmail.com>
2022-08-27 23:21:12 -0500

    Scrimba, Learn React, Build a React Info Site, C008 - JSX

--- M1_BuildAReactInfoSite/index.js
+++ M1_BuildAReactInfoSite/index.js
@@ -1,21 +1,72 @@
-// ! Declarative Way
-ReactDOM.render(
-    <h1 className="header">This is a declarative way to program</h1>,
-    document.getElementById("root")
-);
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// <h1 class="header">Hello World</h1>
+
+// const element = <h1 className="header">This is JSX</h1>;
+// console.log(element);
+
+/*
+    {
+        type: "h1", 
+        key: null, 
+        ref: null, 
+        props: {
+            className: "header", 
+            children: "This is JSX"
+        }, 
+        _owner: null, 
+        _store: {}
+    }
+ */
+
+// JSX
+// ReactDOM.render(element, document.getElementById("root"));
+
+
+
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// );
+
+// // JSX
+// ReactDOM.render(
+//     page, 
+//     document.getElementById("root")
+// );
+
+// * If we need to show many elements, we need to wrap them into a div, since render needs only one parent element and doesn't matter if that parent element has children elements
+
+
+
 
 /* 
-*Challenge - recreate the above line of code in vanilla JS by creating and appending an h1 to our div#root (without using innerHTML).
+Challenge: 
 
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
 */
 
-// ! Imperative Way
-// const h1 = document.createElement("h1");
-// h1.textContent = "This is an imperative way to program";
-// h1.className = "header";
-// document.getElementById("root").append(h1);
+const navbar = (
+    <nav>
+        <h1>ERAS DEV</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
