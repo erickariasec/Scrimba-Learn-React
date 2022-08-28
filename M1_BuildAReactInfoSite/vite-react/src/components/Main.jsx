cc834b164d4c88e1b372947cf827dae681c13147
erickariasec <erickarias1999@hotmail.com>
2022-08-28 18:20:42 -0500

    Scrimba, Learn React, Build a React Info Site, vite-react, C025 - Main Section

--- M1_BuildAReactInfoSite/vite-react/src/components/Main.jsx
+++ M1_BuildAReactInfoSite/vite-react/src/components/Main.jsx
@@ -1,6 +1,17 @@
+import "./Main.css";
+
 function Main() {
     return(
-        <h1>Main goes here</h1>
+        <main className="main">
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     );
 }
 
