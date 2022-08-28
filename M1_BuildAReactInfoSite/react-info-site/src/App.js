924bb441c3e288c5b280b178c0a1cebd69ca237d
erickariasec <erickarias1999@hotmail.com>
2022-08-28 00:42:59 -0500

    Scrimba, Learn React, Build a React Info Site, C010 - Thought experiment: use.append() instead of ReactDOM.render()?

--- M1_BuildAReactInfoSite/react-info-site/src/App.js
+++ M1_BuildAReactInfoSite/react-info-site/src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <nav>
-                <h1>Bob's Bistro</h1>
-                <ul>
-                    <li>Menu</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <div>
+                <h1>My awesome website in React</h1>
+                <h3>Reasons I love React</h3>
+                <ol>
+                    <li>It's composable</li>
+                    <li>It's declarative</li>
+                    <li>It's a hireable skill</li>
+                    <li>It's actively maintained by skilled people</li>
+                </ol>
+            </div>
         </div>
     );
 }
