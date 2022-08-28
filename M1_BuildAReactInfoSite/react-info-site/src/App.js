cf607beedac4812338695146ab3ba8d83a861d6c
erickariasec <erickarias1999@hotmail.com>
2022-08-28 00:01:03 -0500

    Scrimba, Learn React, Build a React Info Site, C009 - Goodbye, CDNs!

---
+++ M1_BuildAReactInfoSite/react-info-site/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <nav>
+                <h1>Bob's Bistro</h1>
+                <ul>
+                    <li>Menu</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default App;
