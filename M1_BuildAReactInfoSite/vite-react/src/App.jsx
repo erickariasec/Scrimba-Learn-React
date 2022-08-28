769541034393aa2b14167bc2b73bedaeb38fa1f8
erickariasec <erickarias1999@hotmail.com>
2022-08-28 18:25:48 -0500

    Scrimba, Learn React, Build a React Info Site, vite-react, C026 - Color the bullets

--- M1_BuildAReactInfoSite/vite-react/src/App.jsx
+++ M1_BuildAReactInfoSite/vite-react/src/App.jsx
@@ -3,12 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 
-/*
-  !Challenge: Complete the Navbar to match the design
 
-  Hint: use the Figma file for the most accurate peek at the design
-  (colors, sizes, fonts, etc.)
-*/
 
 function App() {
   const [count, setCount] = useState(0)
