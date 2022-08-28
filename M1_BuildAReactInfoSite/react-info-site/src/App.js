a6c670e45b373db48dafa06c9bd67f359eee6ad9
erickariasec <erickarias1999@hotmail.com>
2022-08-28 13:29:37 -0500

    Scrimba, Learn React, Build a React Info Site, C019 - Organize Components

--- M1_BuildAReactInfoSite/react-info-site/src/App.js
+++ M1_BuildAReactInfoSite/react-info-site/src/App.js
@@ -5,11 +5,7 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
 /*
-    !Challenge: 
-    *- Add an `ul` inside the Header's `nav` and create the following `li`s: "Pricing", "About", & "Contact"
-    *- Using flexbox, line up the nav items horizontally, and put them inline with the React logo.
-    *- Change the image styling to happen in CSS instead of in-line
-    ?For practice, add a new class to the image in order to style it
+    !Challenge: move the Footer and MainContent components into their own files.
  */
 
 function App() {
