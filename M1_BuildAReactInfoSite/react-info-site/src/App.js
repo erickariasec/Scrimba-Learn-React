ea245b67fc1893255f0c45074be1022b18463b6b
erickariasec <erickarias1999@hotmail.com>
2022-08-28 01:05:01 -0500

    Scrimba, Learn React, Build a React Info Site, C012 - Project part 1 - markup

--- M1_BuildAReactInfoSite/react-info-site/src/App.js
+++ M1_BuildAReactInfoSite/react-info-site/src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
+import "./logo192.png"
+
+// !Challenge: Starting from scratch, build and render the HTML for our section project. Check the Google slide for what you're trying to build.
+
+// We'll be adding styling to it later.
+
+// Hints:
+// * The React logo is a file in the project tree, so you can access it by using `src="./react-logo.png" in your image element
+// * You can also set the `width` attribute of the image element just like in HTML. In the slide, I have it set to 40px
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h1>My awesome website in React</h1>
-                <h3>Reasons I love React</h3>
-                <ol>
-                    <li>It's composable</li>
-                    <li>It's declarative</li>
-                    <li>It's a hireable skill</li>
-                    <li>It's actively maintained by skilled people</li>
-                </ol>
-            </div>
+        <div>
+            <img src="logo192.png" width="50" alt="react-logo"/>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is mantained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     );
 }
