c711477237354781d52c2c05dc67253346ccfabb
erickariasec <erickarias1999@hotmail.com>
2022-08-28 17:13:29 -0500

    Scrimba, Learn React, Build a React Info Site, vite-react, C022 - Project Setup

--- M1_BuildAReactInfoSite/vite-react/src/App.jsx
+++ M1_BuildAReactInfoSite/vite-react/src/App.jsx
@@ -2,34 +2,31 @@ import { useState } from 'react'
 import scrimbaLogo from './assets/scrimba-dark-logo.svg'
 import pizzaImage from './assets/pizza.png'
 import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
+/*
+  !Challenge: Project setup
+
+  - Create a `components` folder
+  - Create the following components in separate files inside the components folder.  In each one, just render an `h1` with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+      - Navbar
+      - Main
+  - Create an App component outside the components folder (sibling to the index.js file)
+      - Have App render the Navbar and Main components
+  - Import and render the App component inside of index.js usin ReactDOM
+      - At this point you should have your "Navbar goes here" etc showing up in the mini-browser.
+  - Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+    Put the links to those fonts ABOVE the style.css link in index.html (Use the `<link/>` elements instead of the @import or npm options for getting the fonts. You may need to do some extra research to figure out how this works if you haven't done it before)
+*/
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={scrimbaLogo} className="logo react" alt="React logo" />
-        </a>
-        <p>Hello Scrimba React Students!</p>
-      </div>
-      <h1>Have a Pizza 🍕</h1>
-      <img src={pizzaImage} className="pizza-img" alt="Pizza" />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Main />
     </div>
   )
 }
