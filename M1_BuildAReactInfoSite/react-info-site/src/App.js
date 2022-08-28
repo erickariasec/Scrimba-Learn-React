ffac3fde130671169c161dfa26c0aea2bef29bde
erickariasec <erickarias1999@hotmail.com>
2022-08-28 11:33:48 -0500

    Scrimba, Learn React, Build a React Info Site, C017 - Parent/Child Components

--- M1_BuildAReactInfoSite/react-info-site/src/App.js
+++ M1_BuildAReactInfoSite/react-info-site/src/App.js
@@ -1,32 +1,24 @@
 import "./App.css";
 import "./logo192.png"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 /*
-    !Challenge: 
+    !Mini Challenge:
+    *Move the `header` element from Page into its own component called "Header"
 
-    * Part 2: 
-    *- Add a `header` element with a nested `nav` element. Inside the `nav`, include a `img` element with the image of the React logo inside (src="./react-logo.png") and make sure to set the width to something more manageable so it doesn't take up the whole screen
-    *- Add an `h1` with some text describing the page. (E.g. "Reasons I'm excited to learn React"). Place it above the ordered list.
-    *- Add a `footer` after the list that says: 
-    *    "© 20xx <last name here> development. All rights reserved."
+    !Challenge:
+    *- Move the `footer` into its own component called "Footer" and render that component inside the Page component.
+    *- Move the `h1` and `ol` together into another component called "MainContent" and render inside Page as well.
  */
 
 function App() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./logo192.png" width="50" alt="react-logo"/>
-                </nav>
-            </header>
-            <h1>Reasons I'm excited about learning React!</h1>
-            <ol>
-                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2022 Arias development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
---
+++ M1_BuildAReactInfoSite/react-info-site/src/components/Header.js
@@ -0,0 +1,13 @@
+import "../logo192.png"
+
+function Header () {
+    return (
+        <header>
+            <nav>
+                <img src="logo192.png" width="40px" alt="react-logo"/>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
