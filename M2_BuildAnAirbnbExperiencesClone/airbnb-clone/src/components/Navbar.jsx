db7376cb601119f515c3cae3d0160921a33b0143
erickariasec <erickarias1999@hotmail.com>
2022-09-05 09:33:30 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C002 - Project Setup: Navbar

---
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/airbnb-logo.png";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="nav">
+            <img className="logo" src={logo} alt="Airbnb Logo" />
+        </nav>
+    );
+}
+
+export default Navbar;
