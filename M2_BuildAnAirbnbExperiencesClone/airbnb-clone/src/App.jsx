003405c886ec9fa3558bd057e5fe42d99cd93fc0
erickariasec <erickarias1999@hotmail.com>
2022-09-05 10:04:35 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C003 - Project Setup: Hero

--- M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero';
 
 /*
-  !Challenge: Build the Navbar component.
+  !Challenge: Build the Hero component.
   Check the Figma file for the design specifics.
 */
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import "./Hero.css"
+import photoGrid from "../assets/photo-grid.png";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <img className="grid-images" src={photoGrid} alt="Airbnb Experiences Photos" />
+            <h1 className="title">Online Experiences</h1>
+            <p className="description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    );
+}
+
+export default Hero;
