da324b1c7998941a46ef1cad54aa7049a7641b2e
erickariasec <erickarias1999@hotmail.com>
2022-09-08 23:31:06 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C008 - Aside JS: inside JSX (simple time of day): adding background color depending on the hour of the day

--- M2_BuildAnAirbnbExperiencesClone/simple-time-of-day/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/simple-time-of-day/src/App.jsx
@@ -5,17 +5,22 @@ function App() {
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay = "";
+  let background_color;
+
 
   if (hours < 12) {
     timeOfDay = "morning";
+    background_color = "linear-gradient(120deg, #f6d365 0%, #fda085 100%)"
   } else if (hours >= 12 && hours <= 17) {
     timeOfDay = "afternoon";
+    background_color = "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)"
   } else {
     timeOfDay = "night";
+    background_color = "linear-gradient(to top, #283E51, #0A2342)"
   }
 
   return (
-    <div id="background-color" className="bg-color">
+    <div className="bg-color" style={{background: background_color}}>
       <h1>Good {timeOfDay}</h1>
     </div>
   )
