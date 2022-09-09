ae499ed4b39f24c6d5b837d660ffc10b42800a71
erickariasec <erickarias1999@hotmail.com>
2022-09-09 16:33:56 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C009 - Props part 3: Create a Contact Component: adding props to each Contact component (cat-card-props app)

--- M2_BuildAnAirbnbExperiencesClone/cat-card-props/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/cat-card-props/src/App.jsx
@@ -10,75 +10,37 @@ import mailIcon from "./assets/mail-icon.png"
 /* 
   !Challenge:
 
-  - Create a Contact.js component in another file
-  - Move one of the contact card divs below into that file
-  - import and render 4 instances of that contact card
-    - Think ahead: what's the problem with doing it this way?
+  - Add all the rest of the data to the Contact card instances
 */
 
 function App() {
   return (
     <div className="App">
       <div className="contacts">
-        <Contact />
-            
-        <Contact />
-        
-        <Contact />
-        
-        <Contact />
-        
-        <div className="contact-card">
-          <img src={mrWhiskerson}/>
-          <h3>Mr. Whiskerson</h3>
-          <div className="info-group">
-            <img src={phoneIcon} />
-            <p>(212) 555-1234</p>
-          </div>
-          <div className="info-group">
-            <img src={mailIcon} />
-            <p>mr.whiskaz@catnap.meow</p>
-          </div>
-        </div>
-        
-        <div className="contact-card">
-          <img src={fluffykins}/>
-          <h3>Fluffykins</h3>
-          <div className="info-group">
-            <img src={phoneIcon} />
-            <p>(212) 555-2345</p>
-          </div>
-          <div className="info-group">
-            <img src={mailIcon} />
-            <p>fluff@me.com</p>
-          </div>
-        </div>
-        
-        <div className="contact-card">
-          <img src={felix}/>
-          <h3>Felix</h3>
-          <div className="info-group">
-            <img src={phoneIcon} />
-            <p>(212) 555-4567</p>
-          </div>
-          <div className="info-group">
-            <img src={mailIcon} />
-            <p>thecat@hotmail.com</p>
-          </div>
-        </div>
-        
-        <div className="contact-card">
-          <img src={pumpkin}/>
-          <h3>Pumpkin</h3>
-          <div className="info-group">
-            <img src={phoneIcon} />
-            <p>(0800) CAT KING</p>
-          </div>
-          <div className="info-group">
-            <img src={mailIcon} />
-            <p>pumpkin@scrimba.com</p>
-          </div>
-        </div>
+        <Contact 
+          img={mrWhiskerson}
+          name="Mr. Whiskerson"
+          phoneNumber="(212) 555-1234"
+          mail="mr.whiskaz@catnap.meow"
+        />
+        <Contact 
+          img={fluffykins}
+          name="Fluffykins"
+          phoneNumber="(212) 555-2345"
+          mail="fluff@me.com"
+        />
+        <Contact 
+          img={felix}
+          name="Felix"
+          phoneNumber="(212) 555-4567"
+          mail="thecat@hotmail.com"
+        />
+        <Contact 
+          img={pumpkin}
+          name="Pumpkin"
+          phoneNumber="(0800) CAT KING"
+          mail="pumpkin@scrimba.com"
+        />
       </div>
     </div>
   )
