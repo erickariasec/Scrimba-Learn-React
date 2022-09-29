09156c6461aeaa9570ee75e045f674a624aec4e2
erickariasec <erickarias1999@hotmail.com>
2022-09-29 18:17:50 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C012 - Destructuring Props

--- M2_BuildAnAirbnbExperiencesClone/cat-card-props/src/components/Contact.jsx
+++ M2_BuildAnAirbnbExperiencesClone/cat-card-props/src/components/Contact.jsx
@@ -1,24 +1,35 @@
 import phoneIcon from "../assets/phone-icon.png"
 import mailIcon from "../assets/mail-icon.png"
 
-function Contact(props) {
-    /**
-        * !Challenge: Fix the code below to use the `props` object values in place of the hardcoded values below
-     */
+/**
+   * !Challenge: fix the bug, now that we've destructured the props object
+*/
+
+function Contact({img, name, phoneNumber, mail}) {
     return (
         <div className="contact-card">
-            <img src={props.img}/>
-            <h3>{props.name}</h3>
+            <img src={img}/>
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src={phoneIcon} />
-                <p>{props.phoneNumber}</p>
+                <p>{phoneNumber}</p>
             </div>
             <div className="info-group">
                 <img src={mailIcon} />
-                <p>{props.mail}</p>
+                <p>{mail}</p>
             </div>
         </div>
     );
 }
 
 export default Contact;
+
+// const person = {
+//     img: "./images/mr-whiskerson.png",
+//     name: "Mr. Whiskerson",
+//     phone: "(800) 555-1234",
+//     email: "mr.whiskaz@catnap.meow"
+// }
+
+// const {img, name} = person
+// console.log(name)
