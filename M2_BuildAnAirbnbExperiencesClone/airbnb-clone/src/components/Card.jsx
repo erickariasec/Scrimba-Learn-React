6301e4deffc6237a5633b2248ab851005fdf1378
erickariasec <erickarias1999@hotmail.com>
2022-09-30 16:59:34 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C025 - Project: Spread object as props

--- M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/components/Card.jsx
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/components/Card.jsx
@@ -3,28 +3,24 @@ import "./Card.css";
 
 export default function Card(props) {
     let badgeText
-    if (props.content.openSpots === 0) {
+    if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.content.location === "Online") {
+    } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
-
-    /*
-        !Challenge: Fix our component! 😱
-    */
 
     return (
         <div className="card">
             {badgeText && <div className="card-badge">{badgeText}</div>}
-            <img src={`/assets/Card/${props.content.coverImg}`} className="card-image" />
+            <img src={`/assets/Card/${props.coverImg}`} className="card-image" />
             <div className="card-stats">
                 <img src={star} className="card-star" />
-                <span>{props.content.stats.rating}</span>
-                <span className="gray">({props.content.stats.reviewCount}) • </span>
-                <span className="gray">{props.content.location}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card-title">{props.content.title}</p>
-            <p className="card-price"><span className="bold">From ${props.content.price}</span> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p className="card-price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     );
 }
