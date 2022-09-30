d187a5aa1e1191a268309f2d2ded10a3e6797c11
erickariasec <erickarias1999@hotmail.com>
2022-09-29 23:00:54 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C017 - React Renders Arrays

--- M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
@@ -4,69 +4,26 @@ import Hero from './components/Hero';
 import Card from './components/Card';
 import profilePictureKatie from "./assets/Card/katie-zaferes.png";
 
+/*
+  !Challenge: turn the strings in the array into <h3> elements instead
+*/
+
 function App() {
+  const colors = [
+    <h3>Red</h3>, 
+    <h3>Orange</h3>, 
+    <h3>Yellow</h3>,
+    <h3>Green</h3>,
+    <h3>Blue</h3>,
+    <h3>Indigo</h3>,
+    <h3>Violet</h3>
+  ]
+
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <Card 
-        img={profilePictureKatie}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {colors}
     </div>
   )
 }
 
 export default App;
-
-/*
-Challenge 1:
-Given an array of numbers, return an array of each number, squared
-*/
-const nums = [1, 2, 3, 4, 5]
-// -->       [1, 4, 9, 16, 25]
-// Your code here
-const squares = nums.map(function(num) {
-    return num * num
-})
-
-console.log(squares)
-
-
-
-/*
-Challenge 2:
-Given an array of strings, return an array where 
-the first letter of each string is capitalized
-*/
-
-const names = ["alice", "bob", "charlie", "danielle"]
-// -->        ["Alice", "Bob", "Charlie", "Danielle"]
-// Your code here
-const capitalized = names.map(name => (
-    name[0].toUpperCase() + name.slice(1))
-)
-console.log(capitalized)
-
-
-
-/*
-Challenge 3:
-Given an array of strings, return an array of strings that wraps each
-of the original strings in an HTML-like <p></p> tag.
-
-E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
-return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
-*/
-
-const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
-// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
-// Your code here
-
-const paragraphs = pokemon.map(monster => `<p>${monster}</p>`)
-
-console.log(paragraphs)
