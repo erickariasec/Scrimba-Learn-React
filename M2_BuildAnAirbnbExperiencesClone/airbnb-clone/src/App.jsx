2bc5d9bb90f269b9f96409aae327ddfdc12ccafb
erickariasec <erickarias1999@hotmail.com>
2022-09-30 00:24:09 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C021 - Project: Map experiences data into components

--- M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
@@ -2,20 +2,37 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import Card from './components/Card';
+import cardData from './cardData';
+
+/*
+  !Challenge:
+
+  - import the array of data from cardData.js
+  - map over the array to create <Card /> components
+  - display the array of card components under the navbar
+    (in place of the current <Card /> component)
+
+  Note: We haven't styled the group of components yet, so they'll
+  still be block elements, stacked vertically. We'll add styling later.
+*/
 
 function App() {
+  const cards = cardData.map(content => {
+    return <Card 
+      img={content.coverImg}
+      rating={content.stats.rating}
+      reviewCount={content.stats.reviewCount}
+      location={content.location}
+      title={content.title}
+      price={content.price}
+    />
+  }) 
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   )
 }
