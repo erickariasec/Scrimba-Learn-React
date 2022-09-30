bf53610dc8a572caf66348d349bbd7ab79b4274d
erickariasec <erickarias1999@hotmail.com>
2022-09-29 23:02:29 -0500

    Scrimba, Learn React, Build and Airbnb Experiences Clone, C017 - React Renders Arrays: RETURN TO NORMAL APP VERSION

--- M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
+++ M2_BuildAnAirbnbExperiencesClone/airbnb-clone/src/App.jsx
@@ -4,24 +4,19 @@ import Hero from './components/Hero';
 import Card from './components/Card';
 import profilePictureKatie from "./assets/Card/katie-zaferes.png";
 
-/*
-  !Challenge: turn the strings in the array into <h3> elements instead
-*/
-
 function App() {
-  const colors = [
-    <h3>Red</h3>, 
-    <h3>Orange</h3>, 
-    <h3>Yellow</h3>,
-    <h3>Green</h3>,
-    <h3>Blue</h3>,
-    <h3>Indigo</h3>,
-    <h3>Violet</h3>
-  ]
-
   return (
     <div className="App">
-      {colors}
+      <Navbar />
+      <Hero />
+      <Card 
+        img={profilePictureKatie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   )
 }
