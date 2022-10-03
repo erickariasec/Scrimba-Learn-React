6666fcc5896b1ff1122d065697bcfe47854d7df5
erickariasec <erickarias1999@hotmail.com>
2022-10-03 14:57:44 -0500

    Scrimba, Learn React, Build a Meme Generator, C015 - useState - Changing state with a callback function

--- M3_BuildAMemeGenerator/counter-app/src/App.jsx
+++ M3_BuildAMemeGenerator/counter-app/src/App.jsx
@@ -4,12 +4,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
     
+  /**
+    * !Note: if you ever need the old value of state
+    * to help you determine the new value of state,
+    * you should pass a callback function to your
+    * state setter function instead of using
+    * state directly. This callback function will
+    * receive the old value of state as its parameter,
+    * which you can then use to determine your new
+    * value of state.
+  */
+ 
   function add() {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1)
   }
   
   function subtract() {
-    setCount(count - 1)
+    setCount(prevCount => prevCount - 1)
   }
 
   return (
