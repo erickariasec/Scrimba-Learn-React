4d7674531eb5277a387010ef05c7d9ea50d2a5ff
erickariasec <erickarias1999@hotmail.com>
2022-10-03 09:09:45 -0500

    Scrimba, Learn React, Build a Meme Generator, C006 - Project: Get random meme

--- M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
+++ M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
@@ -1,7 +1,25 @@
+import memesData from "../memesData"
+
 export default function Meme() {
+    /**
+        * !Challenge: Get a random image from the `memesData` array when the "new meme image" button is clicked.
+        * 
+        * Log the URL of the image to the console. (Don't worry
+        * about displaying the image yet)
+    */
+
+    let memeUrlImg
+
+    const getMemeImage = () => {
+        const memesArray = memesData.data.memes 
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        memeUrlImg = memesArray[randomNumber].url
+        console.log(memeUrlImg)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -14,10 +32,11 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
