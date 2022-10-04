d13dff7c2f700b6a4aae0721b57091410ccc65be
erickariasec <erickarias1999@hotmail.com>
2022-10-04 18:49:13 -0500

    Scrimba, Learn React, Build a Meme Generator, C048 - Forms in React: Select & Option

--- M3_BuildAMemeGenerator/basic-form-react/src/components/Form.jsx
+++ M3_BuildAMemeGenerator/basic-form-react/src/components/Form.jsx
@@ -8,9 +8,12 @@ export default function Form() {
             email: "",
             comments: "",
             isFriendly: true,
-            employment: ""
+            employment: "",
+            favColor: ""
         }
     )
+
+    console.log(formData.favColor)
 
     function handleChange(event) {
         const {name, value, type, checked} = event.target
@@ -99,6 +102,25 @@ export default function Form() {
                 <br />
                 
             </fieldset>
+            <br />
+            
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="">-- Choose --</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
         </form>
     )
 }
