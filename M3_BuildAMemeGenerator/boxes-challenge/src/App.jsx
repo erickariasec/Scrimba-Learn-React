6a71664fb8bb5c130f1755a7184a5bbd2b42e42f
erickariasec <erickarias1999@hotmail.com>
2022-10-04 08:55:30 -0500

    Scrimba, Learn React, Build a Meme Generator, C028 - Dynamic Styles

--- M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
+++ M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import boxes from './boxes'
 
-function App() {
+function App(props) {
 
   /**
-    * !Challenge part 1:
-    * 1. Initialize state with the default value of the
-    *    array pulled in from boxes.js
-    * 2. Map over that state array and display each one
-    *    as an empty square (black border, transparent bg color)
-    *    (Don't worry about using the "on" property yet)
+    // !Challenge: use a ternary to determine the backgroundColor.
+    // If darkMode is true, set it to "#222222"
+    // If darkMode is false, set it to "#cccccc"
   */
 
   const [squares, setSquares] = useState(boxes)
 
+  const styles = {
+    backgroundColor: props.darkMode ? "#222222" : "#cccccc",
+    borderRadius: props.darkMode ? "50%" : "5px",
+    borderColor: props.darkMode ? "crimson" : "yellow"
+  }
+
   const squareElements = squares.map(square => {
-    return <div className="box" key={square.id}></div>
+    return <div style={styles} className="box" key={square.id}></div>
   })
 
   return (
