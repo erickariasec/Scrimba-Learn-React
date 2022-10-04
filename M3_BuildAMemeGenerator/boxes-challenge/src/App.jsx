a6f6e9e4a50ae1ea67bf8f5a044041d9b1f13f4e
erickariasec <erickarias1999@hotmail.com>
2022-10-04 09:58:55 -0500

    Scrimba, Learn React, Build a Meme Generator, C030 - Boxes Challenge Part 3.1 - local state

--- M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
+++ M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
@@ -5,18 +5,12 @@ import Box from './components/Box'
 
 function App() {
 
-  /**
-    // !Challenge: use a ternary to determine the backgroundColor.
-    // If darkMode is true, set it to "#222222"
-    // If darkMode is false, set it to "#cccccc"
-  */
-
   const [squares, setSquares] = useState(boxes)
 
   const squareElements = squares.map(square => (
     <Box 
-      on={square.on}
       key={square.id}
+      on={square.on}
     />
   ))
 
