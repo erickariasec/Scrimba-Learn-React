26dbd18f4009822264b57e8fbaf08fac3e7cc29e
erickariasec <erickarias1999@hotmail.com>
2022-10-04 10:31:17 -0500

    Scrimba, Learn React, Build a Meme Generator, C031 - Boxes Challenge Part 3.2 - Unified State

--- M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
+++ M3_BuildAMemeGenerator/boxes-challenge/src/App.jsx
@@ -7,10 +7,24 @@ function App() {
 
   const [squares, setSquares] = useState(boxes)
 
+  /**
+    * !Challenge: Create a toggle() function that logs
+    * "clicked!" to the console
+    * 
+    * Pass that function down to each of the Box components
+    * and set it up so when they get clicked it runs the function
+  */
+
+  function toggle(id) {
+    console.log(id)
+  }
+
   const squareElements = squares.map(square => (
     <Box 
       key={square.id}
+      id={square.id}
       on={square.on}
+      handleClick={toggle}
     />
   ))
 
--- M3_BuildAMemeGenerator/boxes-challenge/src/components/Box.jsx
+++ M3_BuildAMemeGenerator/boxes-challenge/src/components/Box.jsx
@@ -1,29 +1,10 @@
-import { useState } from "react";
-
 export default function Box(props) {
 
-    /**
-        * !Challenge: Create state controlling whether
-        * this box is "on" or "off". Use the incoming
-        * `props.on` to determine the initial state.
-        * 
-        * Create an event listener so when the box is clicked,
-        * it toggles from "on" to "off".
-        * 
-        * Goal: clicking each box should toggle it on and off.
-    */
-
-    const [onBox, setOnBox] = useState(props.on)
-
     const styles = {
-        backgroundColor: onBox ? "#222222" : "transparent"
-    }
-
-    function toggle() {
-        setOnBox(prevOnBox => !prevOnBox)
+        backgroundColor: props.on ? "#222222" : "transparent"
     }
 
     return (
-        <div style={styles} className="box" onClick={toggle}></div>
+        <div style={styles} className="box" onClick={() => props.handleClick(props.id)}></div>
     );
 }
