cc679646c5c20807d91818c68db05ce1b295ad26
erickariasec <erickarias1999@hotmail.com>
2022-10-05 09:39:11 -0500

    Scrimba, Learn React, Build a Meme Generator, C056 - useEffect dependencies array

--- M3_BuildAMemeGenerator/making-api-calls/src/App.jsx
+++ M3_BuildAMemeGenerator/making-api-calls/src/App.jsx
@@ -3,20 +3,25 @@ import './App.css'
 
 function App() {
   const [starWarsData, setStarWarsData] = useState({})
+  const [count, setCount] = useState(0)
 
   console.log("Component rendered")
-    
-        
-  // side effects
-  useEffect(function() {
-    fetch("https://swapi.dev/api/people/1")
-      .then(res => res.json())
-      // .then(data => setStarWarsData(data))
-  }, ???)
+      
+  /**
+    * !Challenge: re-write the useEffect
+    * It should run any time `count` changes
+    * For now, just console.log("Effect function ran")
+  */
+
+  useEffect(() => {
+    console.log("Effect function ran")
+  }, [count]) // dependencies array (rerender each time count changes)
 
   return (
     <div className="App">
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
     </div>
   )
 }
