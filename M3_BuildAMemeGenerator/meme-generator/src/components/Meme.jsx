745963c1e9bfa5b19092c6f88c843e5f100d9604
erickariasec <erickarias1999@hotmail.com>
2022-10-04 22:52:54 -0500

    Scrimba, Learn React, Build a Meme Generator, C052 - Project: Add text to image

--- M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
+++ M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
@@ -4,20 +4,11 @@ import allMemeImages from "../memesData"
 
 export default function Meme() {
     /**
-        * !Challenge: Update our state to save the meme-related
-        * data as an object called `meme`. It should have the
-        * following 3 properties:
-        * topText, bottomText, randomImage.
-        * 
-        * The 2 text states can default to empty strings for now,
-        * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-        * 
-        * Next, create a new state variable called `allMemeImages`
-        * which will default to `memesData`, which we imported above
-        * 
-        * Lastly, update the `getMemeImage` function and the markup 
-        * to reflect our newly reformed state object and array in the
-        * correct way.
+        * !Challenge: 
+        * 1. Set up the text inputs to save to
+        *    the `topText` and `bottomText` state variables.
+        * 2. Replace the hard-coded text on the image with
+        *    the text being saved to state.
     */
 
     const [memeImage, setMemeImage] = useState(
@@ -40,6 +31,16 @@ export default function Meme() {
         }))
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMemeImage(prevMemeImage => {
+            return {
+                ...prevMemeImage,
+                [name] : value
+            }
+        })
+    }
+
     return (
         <main>
             <div className="form">
@@ -47,11 +48,17 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    onChange={handleChange}
+                    name="topText"
+                    value={memeImage.topText}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    onChange={handleChange}
+                    name="bottomText"
+                    value={memeImage.bottomText}
                 />
                 <button 
                     className="form--button"
@@ -60,7 +67,11 @@ export default function Meme() {
                     Get a new meme image
                 </button>
             </div>
-            <img src={memeImage.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={memeImage.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+            </div>
         </main>
     )
 }
