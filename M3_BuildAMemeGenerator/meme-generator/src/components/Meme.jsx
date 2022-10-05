f4689ac54864f55832f4e205bb29360f0d769963
erickariasec <erickarias1999@hotmail.com>
2022-10-05 18:42:38 -0500

    Scrimba, Learn React, Build a Meme Generator, C063 - Using an Async Function inside useEffect (Clone)

--- M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
+++ M3_BuildAMemeGenerator/meme-generator/src/components/Meme.jsx
@@ -1,20 +1,15 @@
 import { useState, useEffect } from "react"
 
 export default function Meme() {
+
     /**
-        * !Challenge: 
-        * As soon as the Meme component loads the first time,
-        * make an API call to "https://api.imgflip.com/get_memes".
-        * 
-        * When the data comes in, save just the memes array part
-        * of that data to the `allMemes` state
-        * 
-        * Think about if there are any dependencies that, if they
-        * changed, you'd want to cause to re-run this function.
-        * 
-        * Hint: for now, don't try to use an async/await function.
-        * Instead, use `.then()` blocks to resolve the promises
-        * from using `fetch`. We'll learn why after this challenge.
+        *!useEffect takes a function as its parameter. If that function
+        *!returns something, it needs to be a cleanup function. Otherwise,
+        *!it should return nothing. If we make it an async function, it
+        *!automatically retuns a promise instead of a function or nothing.
+        *!Therefore, if you want to use async operations inside of useEffect,
+        *!you need to define the function separately inside of the callback
+        *!function, as seen below:
     */
 
     const [memeImage, setMemeImage] = useState(
@@ -28,10 +23,17 @@ export default function Meme() {
     const [allMemeImages, setAllMemeImages] = useState([])
 
     useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemeImages(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        getMemes()
     }, [])
+    
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemeImages(data.data.memes))
 
     const getMemeImage = () => {
         const randomNumber = Math.floor(Math.random() * allMemeImages.length)
