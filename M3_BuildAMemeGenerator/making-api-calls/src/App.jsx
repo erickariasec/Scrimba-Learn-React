abecbbdb8ab1a9c086c8518b1fab8875b161594b
erickariasec <erickarias1999@hotmail.com>
2022-10-05 10:13:00 -0500

    Scrimba, Learn React, Build a Meme Generator, C058 - useEffect for fetching data

--- M3_BuildAMemeGenerator/making-api-calls/src/App.jsx
+++ M3_BuildAMemeGenerator/making-api-calls/src/App.jsx
@@ -5,17 +5,21 @@ function App() {
   const [starWarsData, setStarWarsData] = useState({})
   const [count, setCount] = useState(0)
 
-  console.log("Component rendered")
-      
   /**
-    * !Challenge: re-write the useEffect
-    * It should run any time `count` changes
-    * For now, just console.log("Effect function ran")
+    * !Quiz:
+    * 1. What will happen if I put back our Star Wars API call
+    *    into the effect function?
+    * 2. How will the useEffect be different if I use 
+    *    setStarWarsData() instead of console.log()
+    * 3. What SHOULD be in our dependencies array in this case?
   */
-
-  useEffect(() => {
-    console.log("Effect function ran")
-  }, [count]) // dependencies array (rerender each time count changes)
+      
+  useEffect(function() {
+    console.log("Effect ran")
+    fetch("https://swapi.dev/api/people/1")
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  }, [])
 
   return (
     <div className="App">
